b13f9b9ffae3a0e6039f4f2ad3bab1b1930bc5ae
Tech Memorials <twcfgdc@gmail.com>
2024-07-14 19:29:48 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// phpinfo();
+// echo ini_get('memory_limit');
+
+$app = include __DIR__ . '/../src/App/bootstrap.php';
+
+
+$app->Run();
---
+++ src/Framework/App.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+namespace Framework;
+
+class App
+{
+      public function Run()
+      {
+            echo "Application is running!";
+      }
+}
